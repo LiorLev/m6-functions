ec0db0d891e638f0c8805d09e6057be9d5df0523
Lior Levy <lior7212@gmail.com>
2017-01-10 18:00:53 -0800

    add solutions to ex 2 and 3

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,20 +1,30 @@
 # Exercise 2: writing and executing functions (II)
 
-# Write a function `CompareLength` that takes in 2 vectors, and returns the sentence:
-# "The difference in lengths is N"
+# Define a function `CompareLength` that takes in 2 character strings, and returns
+# the sentence of the form
+#   "The difference in length is N"
 
+CompareLength <- function(string1, string2){
+  difference <- abs(nchar(string1)-nchar(string2))
+  sentence <- paste("The difference in lenfth is", difference)
+  return(sentence)
+}
 
-# Pass two vectors of different length to your `CompareLength` function
+# Pass two strings of different lengths to your `CompareLength` function
+CompareLength("hello", "how")
 
+# Define a function `DescribeDifference` that will return one of the following statements:
+# "Your first string is longer by N characters"
+# "Your second string is longer by N characters"
 
-# Write a function `DescribeDifference` that will return one of the following statements:
-# "Your first vector is longer by N elements"
-# "Your second vector is longer by N elements"
-
-
-# Pass two vectors to your `DescribeDifference` function
-
-
-### Bonus ###
-
-# Rewrite your `DescribeDifference` function to tell you the name of the vector which is longer
+DescribeDifference <- function(string1, string2){
+  difference <- nchar(string1)-nchar(string2)
+  if(difference>0){
+    description <- paste("Your first string is longer by", difference, "characters")
+  }else if(difference<0){
+     description <- paste("Your second string is longer by", abs(difference), "characters" )
+  }
+  return(description)
+}
+# Pass two strings of different lengths to your `DescribeDifference` function
+DescribeDifference("hey", "what")
